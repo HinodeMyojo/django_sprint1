ae8b236ae06af6bbfc73756e955e2fa189e51be1
Hinode Myojo <Hinodem@mail.ru>
2023-11-01 00:21:15 +0300

    working project

---
+++ blogicum/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def about(request):
+    templates = 'pages/about.html'
+    return render(request, templates)
+
+def rules(request):
+    templates = 'pages/rules.html'
+    return render(request, templates)
+
