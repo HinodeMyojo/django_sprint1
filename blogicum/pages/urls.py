2c495321a18b91efab903d2d06b5e14fd473c05b
Hinode Myojo <Hinodem@mail.ru>
2023-11-04 22:13:36 +0300

    commit

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -7,11 +7,12 @@ which is located in the 'views' file, assign name=rules to it.
 
 from django.urls import path
 from django.views.generic import TemplateView
-from . import views
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-    path('rules/', TemplateView.as_view(template_name='pages/rules.html'), name='rules')
+    path('about/', TemplateView.as_view(template_name='pages/about.html'),
+         name='about'),
+    path('rules/', TemplateView.as_view(template_name='pages/rules.html'), 
+         name='rules')
 ]
